35aaceed4cae0b85f2b29a5af76f5bffeb5d0d01
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2019-07-24 16:33:22 +0200

    added more sanity tests

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -34,3 +34,27 @@ test_that("Maximum likelihood sanity",{
   # plot(rhoRange,ln) visually checked against figure 2 of https://doi.org/10.3102%2F10769986027003223
   expect_equal(which.max(lh),1)
 })
+
+test_that("Positive and nonpositive are the same",{
+  normalEstimators <- setdiff(names(normalRes)[!grepl("*_Positive",names(normalRes))],"Maximum_Likelihood")
+  for (estimator in normalEstimators){
+    expect_equivalent(normalRes[estimator],normalRes[paste0(estimator,"_Positive")])
+  }
+})
+
+test_that("Positive versions are positive",{
+  set.seed(9041249)
+  testData <- MyDataGeneration(12,0,0,10,TRUE)
+  testData <- as.data.frame(cbind(testData$y,testData$X))
+  x <- lm(V1 ~ .,data=testData)
+  normalRes <- altR2(x)
+  posEstimators <- grepl("*_Positive",names(normalRes))
+  expect(all(posEstimators)>=0,"not all positive estimators >0")
+})
+
+test_that("All different",{
+  normalEstimators <- names(normalRes)[!grepl("*_Positive",names(normalRes))]
+  for (estimator in normalEstimators){
+    expect(all(normalRes[estimator] != normalRes[setdiff(normalEstimators,estimator)]),sprintf('%s is equal to at least one other estimator',estimator))
+  }
+})
