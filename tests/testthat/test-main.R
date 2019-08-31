2ce9be2cbda2c01e5487d66f0bada89521cbf94f
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2019-08-31 18:53:06 +0200

    make gsl dependency less restrict; add more tests for new estimator

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -59,3 +59,57 @@ test_that("All different",{
     expect(all(normalRes[estimator] != normalRes[setdiff(normalEstimators,estimator)]),sprintf('%s is equal to at least one other estimator',estimator))
   }
 })
+
+test_that("new Estimator more elaborate",{
+  julsSeq <- function(from,to,by){
+    if (to<from){
+      return(integer())
+    }else{
+      return(seq(from=from,to=to,by=by))
+    }
+  }
+
+  julsHypergeo <- function(a,b,c,z){
+    stopifnot(a==1)
+    stopifnot(b==1)
+    stopifnot(c %% 0.5 == 0)
+    #see http://functions.wolfram.com/HypergeometricFunctions/Hypergeometric2F1/03/06/07/02/0005/
+    #works for c=3. if not not. with numerical problems
+    if (c %% 1==0){
+      prefactor <- (c-1)*z*(z-1)^{-2}
+      theSum<-0
+      shared <- ((z-1)/z)
+      for (k in julsSeq(from=2,to=(c-1),by=1)){
+        term1 <- shared^k/(c-k)
+        # print(sprintf('theSum %f, term1 %f',theSum,term1))
+        theSum <- theSum+term1
+      }
+      result <- prefactor*(theSum-shared^c*log(1-z))
+    }else{
+      # see https://mathoverflow.net/questions/335424/evaluate-gaussian-hypergeometric-function-2f-111cz?noredirect=1#comment837983_335424
+      # works with numerical problems
+      curRes <- 1/(1-z)*(1+(sqrt(z)*asin(sqrt(z)))/sqrt(1-z))
+      for (i in julsSeq(from=1.5,to=c,by=1)){
+        curC <- i-1
+        # print(sprintf('My %f, Gnu %f',curRes,hyperg_2F1(1,1,curC,as.double(z))))
+        curRes <- (curC-curC*(1-z)*curRes)/(z*(curC-1))
+      }
+      result <- curRes
+    }
+    return(result)
+  }
+
+  OPExactEstimatorJul <- function(Rsquared,N,p){
+    factor1 <- (N-3)/(N-p-1)*(1-Rsquared)
+    factor2 <- julsHypergeo(1,1,(N-p+1)/2,1-Rsquared)
+    res <- 1-factor1*factor2
+    return(res)
+  }
+
+  N <- 10
+  p <- 8
+  rsquaredRange <- seq(from=0.01,by=0.01,to=0.99)
+  for (rsquared in rsquaredRange){
+    expect_equivalent(OPExactEstimatorJul(rsquared,N,p),OPExactEstimator(rsquared,N,p),tolerance=10^-5)
+  }
+})
